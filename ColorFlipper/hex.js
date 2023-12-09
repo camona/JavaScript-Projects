c74cbf0c438ecc425873ab8ef830ec8754b276fc
Caroline Moland Naterstad <103410211+camona@users.noreply.github.com>
2023-12-09 14:15:29 +0100

    Update hex.js

--- ColorFlipper/hex.js
+++ ColorFlipper/hex.js
@@ -1,1 +1,13 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.querySelector("#btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function() {
+    const randomNumbers = getRandomNumbers();
+    document.body.style.backgroundColor = color[randomNumbers];
+    color.textContent = color[randomNumbers];
+});
+
+function getRandomNumbers() {
+    
+}
