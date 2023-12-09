33b46b2934b2f278211c95cb8e03f8f007ae07e2
Caroline Moland Naterstad <103410211+camona@users.noreply.github.com>
2023-12-09 14:36:27 +0100

    Update simple.js

--- ColorFlipper/simple.js
+++ ColorFlipper/simple.js
@@ -3,7 +3,6 @@ const btn = document.querySelector("#btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener("click", function() {
-    // get random numbers between 0 and 3
     const randomNumber = getRandomNumber();
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
